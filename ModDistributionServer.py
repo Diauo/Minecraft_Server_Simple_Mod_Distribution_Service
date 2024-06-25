21cf59520f7660c86a5bb26796a9c40d568b71c9
蜂巢集群 [炎黄] <2684674727@qq.com>
2024-06-25 22:07:07 +0800

    初始化提交
    初始化提交

---
+++ ModDistributionServer.py
@@ -0,0 +1,125 @@
+from flask import Flask, jsonify, send_from_directory, send_file
+import os
+import hashlib
+import time
+import logging
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+from apscheduler.schedulers.background import BackgroundScheduler
+
+app = Flask("Minecraft MOD分发服务")
+MODS_DIR = '/root/minecraft-1.20.1-forge-server/mods'
+CLIENT_MODS_DIR = '/root/minecraft-1.20.1-forge-server/clientMods'
+mod_cache = {}
+last_updated = 0
+
+# 初始化日志目录
+log_directory = os.path.join(os.getcwd(), 'log')
+if not os.path.exists(log_directory):
+    os.makedirs(log_directory)
+
+# 配置日志记录器
+log_file = os.path.join(log_directory, 'app.log')
+
+# 添加控制台日志处理器
+console_handler = logging.FileHandler(log_file, encoding='utf-8')
+console_handler.setLevel(logging.DEBUG)
+console_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s'))
+
+# 获取Flask的默认logger并添加处理器
+app.logger.addHandler(console_handler)
+
+# 设置基本配置，防止重复记录
+logging.basicConfig(level=logging.DEBUG)
+
+# 确保Flask logger使用与基本配置一致的等级
+app.logger.setLevel(logging.DEBUG)
+
+scheduler = BackgroundScheduler()
+# 每10分钟运行一次
+scheduler.add_job(func=update_mod_cache, trigger="interval", minutes=10)
+scheduler.start()
+
+# 计算MD5
+def calculate_md5(file_path):
+    md5 = hashlib.md5()
+    with open(file_path, 'rb') as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            md5.update(chunk)
+    return md5.hexdigest()
+
+# 更新MD5缓存信息
+def update_mod_cache():
+    app.logger.info('>> 更新mod信息')
+    global mod_cache
+    mod_cache = {}
+
+    # 更新双端mod的缓存
+    for mod in os.listdir(MODS_DIR):
+        if os.path.isfile(os.path.join(MODS_DIR, mod)) and mod.endswith('.jar') and not mod.startswith('!'):
+            md5 = calculate_md5(os.path.join(MODS_DIR, mod))
+            mod_cache[md5] = {
+                'name': mod ,
+                'type': 'dual'
+            }
+
+    # 更新纯客户端mod的缓存
+    for mod in os.listdir(CLIENT_MODS_DIR):
+        if os.path.isfile(os.path.join(CLIENT_MODS_DIR, mod)) and mod.endswith('.jar'):
+            md5 = calculate_md5(os.path.join(CLIENT_MODS_DIR, mod))
+            mod_cache[md5] = {
+                'name': mod,
+                'type': 'client'
+            }
+
+class ModDirEventHandler(FileSystemEventHandler):
+    def on_modified(self, event):
+        global last_updated
+        if time.time() - last_updated < 1:
+            return
+        last_updated = time.time()
+        update_mod_cache()
+
+# 重新更新缓存
+@app.route('/update-cache', methods=['GET'])
+def mod_list():
+    app.logger.info('>> 进行缓存更新')
+    update_mod_cache()
+    return "成功", 200
+    
+# 获取mod列表
+@app.route('/mod-list', methods=['GET'])
+def mod_list():
+    app.logger.info('>> 获取mod列表')
+    mod_list = [{'name': value['name'], 'md5': md5 } for md5, value in mod_cache.items()]
+    return jsonify(mod_list)
+
+# 下载mod
+@app.route('/mods/<md5>', methods=['GET'])
+def download_mod(md5):
+    app.logger.info(f">> 获取mod：{md5}")
+    mod_info = mod_cache.get(md5)
+    if mod_info:
+        app.logger.info(f">> 下载mod：{mod_info['name']}")
+        directory = CLIENT_MODS_DIR if mod_info['type'] == 'client' else MODS_DIR
+        file_path = os.path.join(directory, mod_info['name'])
+        return send_from_directory(directory, mod_info['name'], as_attachment=True)
+    return "未找到mod", 404
+
+if __name__ == '__main__':
+    # 启动时更新mod信息
+    app.logger.info('> 初始化mod信息')
+    update_mod_cache()
+    app.logger.info('> 部署看门狗')
+    event_handler = ModDirEventHandler()
+    observer = Observer()
+    observer.schedule(event_handler, MODS_DIR, recursive=False)
+    observer.schedule(event_handler, CLIENT_MODS_DIR, recursive=False)
+    observer.start()
+    try:
+        app.logger.info('> 初始化完成')
+        app.run(host='0.0.0.0', port=23432)
+    finally:
+        app.logger.info('> 服务断开')
+        observer.stop()
+        observer.join()
---
+++ client.py
@@ -0,0 +1,151 @@
+import requests
+import os
+import hashlib
+import json
+import re
+
+SERVER_URL = ""
+CLIENT_MODS_DIR = ""
+config_filename = "modUpdateConfig.json"
+
+
+def init():
+    global CLIENT_MODS_DIR
+    global SERVER_URL
+
+    if not os.path.exists(config_filename):
+        # 文件不存在，创建并设置初始值
+        config_data = {}
+
+        # 要求用户输入一个文件路径和URL
+        path = input("请输入Minecraft mod目录: ")
+        url = input("请输入Mod分发服务地址(回车使用默认): ")
+        if not url:
+            url = "http://t.rainplay.cn:46179"
+
+        config_data["path"] = path
+        config_data["url"] = url
+
+        CLIENT_MODS_DIR = path
+        SERVER_URL = url
+
+        # 将数据写入modUpdateConfig.json
+        with open(config_filename, "w", encoding="utf-8") as config_file:
+            json.dump(config_data, config_file, indent=4)
+    else:
+        # 文件存在，从中读取路径和URL
+        with open(config_filename, "r", encoding="utf-8") as config_file:
+            config_data = json.load(config_file)
+
+        path = config_data.get("path", None)
+        url = config_data.get("url", None)
+
+        if path and url:
+            if not os.path.exists(path):
+                print("配置文件中的地址无效，请确认")
+                path = input("请输入Minecraft mod目录: ")
+                if not os.path.exists(path):
+                    input("路径无效！程序终止（回车结束）")
+                    exit()
+                config_data["path"] = path
+                # 将数据写入modUpdateConfig.json
+                with open(config_filename, "w", encoding="utf-8") as config_file:
+                    json.dump(config_data, config_file, indent=4)
+            CLIENT_MODS_DIR = path
+            SERVER_URL = url
+            print(f"配置加载成功: \nMinecraft mod目录: {path} \nMod分发服务地址: {url}")
+        else:
+            if not path:
+                print(f"从配置文件中读取Minecraft mod目录失败")
+                path = input("请输入Minecraft mod目录: ")
+                config_data["path"] = path
+                CLIENT_MODS_DIR = path
+
+            if not url:
+                print(f"从配置文件中读取Mod分发服务地址失败")
+                url = input("请输入Mod分发服务地址: ")
+                config_data["url"] = url
+                SERVER_URL = url
+
+            # 更新配置文件
+            with open(config_filename, "w", encoding="utf-8") as config_file:
+                json.dump(config_data, config_file, indent=4)
+
+
+def get_mod_list():
+    response = requests.get(f"{SERVER_URL}/mod-list")
+    return (
+        {mod["md5"]: mod["name"] for mod in response.json()}
+        if response.status_code == 200
+        else []
+    )
+
+
+def download_mod(mod_md5, mod_name):
+    response = requests.get(f"{SERVER_URL}/mods/{mod_md5}")
+    mod_name = re.sub(r'[\\/:*?"<>|]', "_", mod_name)
+    if response.status_code == 200:
+        with open(os.path.join(CLIENT_MODS_DIR, mod_name), "wb") as f:
+            f.write(response.content)
+
+
+def del_mod(mod_name):
+    os.remove(os.path.join(CLIENT_MODS_DIR, mod_name))
+
+
+def calculate_md5(file_path):
+    md5 = hashlib.md5()
+    with open(file_path, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            md5.update(chunk)
+    return md5.hexdigest()
+
+
+def get_local_mods():
+    return {
+        calculate_md5(os.path.join(CLIENT_MODS_DIR, mod)): mod
+        for mod in os.listdir(CLIENT_MODS_DIR)
+        if mod.endswith(".jar") and not mod.startswith("!")
+    }
+
+
+def sync_mods():
+    server_mods = get_mod_list()
+    if not server_mods:
+        input(f"从{SERVER_URL}读取mod信息失败，程序终止（回车结束）")
+        exit()
+    local_mods = get_local_mods()
+
+    for md5 in server_mods:
+        if md5 not in local_mods:
+            name = server_mods[md5]
+            print(f"更新mod: {name}")
+            download_mod(md5, name)
+
+    print("删除过时mod。。。")
+    select = ""
+    for md5 in local_mods:
+        if md5 not in server_mods:
+            name = local_mods[md5]
+            if select == "A":
+                print(f"删除mod: {name}")
+                del_mod(name)
+            else:
+                select = input(
+                    f"{name} 已经过期，是否删除？Y=删除，N=保留，A=全部删除 \n>:"
+                )
+                if (select == "A" or select == "a") or (select == "Y" or select == "y"):
+                    print(f"删除mod: {name}")
+                    del_mod(name)
+                else:
+                    continue
+
+
+if __name__ == "__main__":
+    try:
+        init()
+        sync_mods()
+        input("更新完成，按回车退出")
+    except Exception as e:
+        print(e)
+        input("程序异常！结束运行")
